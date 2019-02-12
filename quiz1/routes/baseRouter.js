aadc7b02915e8bc74ea231de24ae5b7b2a3aeb0e
Amir Helmy <amir.helmy@mail.mcgill.ca>
2019-02-12 12:14:06 -0800

    Refactored sign-in page

--- quiz1/routes/baseRouter.js
+++ quiz1/routes/baseRouter.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/", (req, res)=>{
+router.get("/sign_in", (req, res)=>{
     res.render("../views/signIn.ejs");
 });
 
@@ -10,12 +10,12 @@ const COOKIE_MAX_AGE = 1000 * 60 * 60 * 24 * 7;
 router.post('/sign_in', (req, res) => {
     const formData = req.body; 
     res.cookie('username', formData.username, { maxAge: COOKIE_MAX_AGE});
-    res.redirect('/');
+    res.redirect('/clucks');
 });
 
 router.post('/sign_out', (req, res) => {
     res.clearCookie('username');
-    res.redirect('/');
+    res.redirect('/clucks');
 });
 
 module.exports = router;
