52c403431cec65e26092df1a0f9b5d4fdccf3066
Amir Helmy <amir.helmy@mail.mcgill.ca>
2019-02-12 09:41:02 -0800

    Set up static assets middleware and added public directory

--- quiz1/app.js
+++ quiz1/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const logger = require('morgan');
+const path = require('path');
 const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(logger('dev'));
 
