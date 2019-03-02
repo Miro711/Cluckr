a78266a7d35277c3d18c8e185258fcfec73cafb9
Amir Helmy <amir.helmy@mail.mcgill.ca>
2019-03-02 14:18:36 -0800

    Generate review model with product:references, run migrations and add validations including belongs_to  and has_many

--- another-amazon/app/models/product.rb
+++ another-amazon/app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+
+    has_many :reviews, dependent: :destroy
+
     validates :title, presence: true, uniqueness: { case_sensitive: false }
     validates :price, numericality: { greater_than: 0}
     validates :description, presence: true, length: { minimum: 10}
