e41bacd4a726c2f9782357b5c854bc381f314e42
Amir Helmy <amir.helmy@mail.mcgill.ca>
2019-03-02 13:51:00 -0800

    Refactor products controller

--- another-amazon/app/controllers/products_controller.rb
+++ another-amazon/app/controllers/products_controller.rb
@@ -1,11 +1,12 @@
 class ProductsController < ApplicationController
 
+    before_action :find_product, only: [:show, :destroy, :edit, :update]
+
     def new
         @product = Product.new
     end
 
     def create
-        product_params = params.require(:product).permit(:title, :description, :price)
         @product = Product.new product_params
         if @product.save
             redirect_to product_path(@product.id)
@@ -15,7 +16,6 @@ class ProductsController < ApplicationController
     end
 
     def show
-        @product = Product.find(params[:id])
     end
 
     def index
@@ -23,23 +23,29 @@ class ProductsController < ApplicationController
     end
 
     def destroy
-        @product = Product.find(params[:id])
         @product.destroy
         redirect_to products_path
     end
 
     def edit
-        @product = Product.find(params[:id])
     end
 
     def update
-        @product = Product.find(params[:id])
-        product_params = params.require(:product).permit(:title, :description, :price)
         if @product.update product_params
             redirect_to product_path(@product.id)
         else
             render :edit
         end
+    end
+
+    private
+
+    def product_params
+        params.require(:product).permit(:title, :description, :price)
+    end
+
+    def find_product
+        @product = Product.find(params[:id])
     end
 
 end
