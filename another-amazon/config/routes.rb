6654150f0e7b91427e1134a22520fac36c260dae
Amir Helmy <amir.helmy@mail.mcgill.ca>
2019-03-02 14:55:21 -0800

    Fix reviews resources to be shallow

--- another-amazon/config/routes.rb
+++ another-amazon/config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :products do
-    resources :reviews, only: [:create, :destroy]
+    resources :reviews, shallow: true, only: [:create, :destroy]
   end
 end
