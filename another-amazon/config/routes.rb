4b83678e96c3eea8708d1dd4421dbdd9ae4f57e1
Amir Helmy <amir.helmy@mail.mcgill.ca>
2019-03-02 20:50:03 -0800

    Generate users controller, users new & create routes & actions, sign up view template and root route

--- another-amazon/config/routes.rb
+++ another-amazon/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get "/", to: "products#index", as: :root
+  resources :users, only: [:new, :create]
   resources :products do
     resources :reviews, shallow: true, only: [:create, :destroy]
   end
